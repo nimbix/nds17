f1bddf46f720b392cd7ef2a746c60b74d05f2221
Stephen Fox <stephen.fox@nimbix.net>
2017-04-24 23:58:00 -0500

    Update script

--- scripts/stylize.sh
+++ scripts/stylize.sh
@@ -9,4 +9,8 @@ cd /usr/local/neural-style/neural-style
 # Note: Output should be set to /data directory (persistant storage)
 th neural_style.lua -backend cudnn -output_image /data/stylized.png $@
 
+if [ $? -gt 0 ]; then
+    echo "Image stylization failed with an unknown error. Please contact the application maintainer for support."
+fi
+
 exit 0
